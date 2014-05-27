718323a4f5a9f6eb20a2dde6e1e09e670d0ba07b
Christopher Giroir <kelsin@valefor.com>
2014-05-26 20:52:32 -0700

    Working with fonts and other backgrounds

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,44 @@
 var gulp = require('gulp');
 var bower = require('gulp-bower');
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
+var changed = require('gulp-changed');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var less = require('gulp-less');
+var minify = require('gulp-minify-css');
+var prefix = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 
 gulp.task('bower', function() {
   bower();
   gulp.src('_vendor/jquery/dist/jquery.js')
-    .pipe(gulp.dest('js/vendor/01-jquery.js'));
+    .pipe(rename('01-jquery.js'))
+    .pipe(gulp.dest('_assets/js/vendor'));
   gulp.src('_vendor/jquery.stellar/jquery.stellar.js')
-    .pipe(gulp.dest('js/vendor/02-jquery.stellar.js'));
+    .pipe(rename('02-jquery.stellar.js'))
+    .pipe(gulp.dest('_assets/js/vendor'));
+});
+
+gulp.task('js', function() {
+  gulp.src(['_assets/js/vendor/*.js','_assets/js/*.js'])
+    .pipe(uglify())
+    .pipe(concat('kelsin.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('less', function() {
+  gulp.src('_assets/less/**/*.less')
+    .pipe(less())
+    .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7", { cascade: true }))
+    .pipe(minify())
+    .pipe(concat('kelsin.css'))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('_assets/less/**/*.less', ['less']);
+  gulp.watch('_assets/js/*.js', ['js']);
 });
 
 gulp.task('init', ['bower']);
+gulp.task('build', ['js', 'less']);
+gulp.task('default', ['build', 'watch']);
