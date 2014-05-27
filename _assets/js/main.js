a218c7e3d9a77393ad78dd86af3b3913a3a2259d
Christopher Giroir <kelsin@valefor.com>
2014-05-26 22:58:21 -0700

    Adding built assets

---
+++ _assets/js/main.js
@@ -0,0 +1,6 @@
+$(function() {
+  $.stellar({ horizontalScrolling: false,
+              positionProperty: 'transform',
+              hideDistantElements: false,
+              responsive: true });
+});
