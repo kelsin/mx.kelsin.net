315f5d8da238e3fd36eae10fd4031b60d4873680
Christopher Giroir <kelsin@valefor.com>
2014-05-29 14:24:43 -0700

    Adding a better footer

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -9,11 +9,11 @@ $(function() {
     if(scroll >= 400) {
       $('#content').css({'margin-top': $('nav').outerHeight() + 'px'});
       $('#content > .post .meta').css({'top': (scroll - 400) + 'px'});
-      $('nav').css({position: 'fixed'});
+      $('header + nav').css({position: 'fixed'});
     } else {
       $('#content').css({'margin-top': '0'});
       $('#content > .post .meta').css({'top': '0'});
-      $('nav').css({position: 'relative'});
+      $('header + nav').css({position: 'relative'});
     }
   });
 
