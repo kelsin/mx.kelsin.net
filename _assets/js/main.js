c52f6c5d6a5668d6f228ecdd96c6bad8bb2f03ed
Christopher Giroir <kelsin@valefor.com>
2014-05-29 12:08:45 -0700

    Fixed post meta

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,11 +5,14 @@ $(function() {
               responsive: true });
 
   $(window).scroll(function() {
-    if($(window).scrollTop() >= 400) {
+    var scroll = $(window).scrollTop();
+    if(scroll >= 400) {
       $('#content').css({'margin-top': $('nav').outerHeight() + 'px'});
+      $('#content > .post .meta').css({'top': (scroll - 400) + 'px'});
       $('nav').css({position: 'fixed'});
     } else {
       $('#content').css({'margin-top': '0'});
+      $('#content > .post .meta').css({'top': '0'});
       $('nav').css({position: 'relative'});
     }
   });
