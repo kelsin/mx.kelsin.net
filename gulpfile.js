542421e91e4fd8d82f02e17fd3fbe7f630c08592
Christopher Giroir <kelsin@valefor.com>
2019-01-02 15:06:28 -0800

    Removing dependent js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
-var bower = require('gulp-bower');
 var changed = require('gulp-changed');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
@@ -9,21 +8,8 @@ var minify = require('gulp-minify-css');
 var prefix = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 
-gulp.task('bower', function() {
-  bower();
-  gulp.src('_vendor/jquery/dist/jquery.js')
-    .pipe(rename('01-jquery.js'))
-    .pipe(gulp.dest('_assets/js/vendor'));
-  gulp.src('_vendor/colorbox/jquery.colorbox.js')
-    .pipe(rename('02-colorbox.js'))
-    .pipe(gulp.dest('_assets/js/vendor'));
-  gulp.src('_vendor/masonry/dist/masonry.pkgd.js')
-    .pipe(rename('03-masonry.js'))
-    .pipe(gulp.dest('_assets/js/vendor'));
-});
-
 gulp.task('js', function() {
-  gulp.src(['_assets/js/vendor/*.js','_assets/js/*.js'])
+  gulp.src(['_assets/js/*.js'])
     .pipe(uglify().on('error', gutil.log))
     .pipe(concat('kelsin.js'))
     .pipe(gulp.dest('js'));
@@ -43,6 +29,5 @@ gulp.task('watch', function() {
   gulp.watch('_assets/js/*.js', ['js']);
 });
 
-gulp.task('init', ['bower']);
 gulp.task('build', ['js', 'less']);
 gulp.task('default', ['build', 'watch']);
